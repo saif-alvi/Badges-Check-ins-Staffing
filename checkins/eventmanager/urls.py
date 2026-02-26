8823436c317ec8ab64b1a149596bcc01d8cac2ce
Saif Alvi <saif-alvi@hotmail.com>
2026-02-26 03:05:43 -0500

    add more views and templates

--- checkins/eventmanager/urls.py
+++ checkins/eventmanager/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name ="index")
-
+    path("", views.index, name ="index"),
+    path("login/",views.login,name="login"),
+    path("signup/",views.signup,name="signup"),
+    path("event_search/",views.event_search,name="event_search"),
+    path("event_signup_form/",views.event_signup_form,name="event_signup_form"),
+    path("event_confirmation/",views.event_confirmation,name="event_confirmation"),
+    path("host_eventpage/",views.host_eventpage,name="host_eventpage"),
 ]
--- checkins/eventmanager/views.py
+++ checkins/eventmanager/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the event management index")
+    template = loader.get_template("eventmanager/index.html")
+    return render(request, "eventmanager/index.html")
 
+def login(request):
+    return HttpResponse("Welcome to the event management platform!")
 
+def signup(request):
+    return HttpResponse("Welcome to the event management platform!")
+
+def event_search(request):
+    return HttpResponse("Welcome to the event management platform!")
+
+def event_signup_form(request):
+    return HttpResponse("Welcome to the event management platform!")
+
+def event_confirmation(request):
+    return HttpResponse("Welcome to the event management platform!")
+
+def host_eventpage(request):
+    return HttpResponse("Welcome to the event management platform!")
